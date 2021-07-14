515ee4aeea3fb4d93cdaf551ec18bbe8563a0180
zxh109030 <358864579@qq.com>
2021-07-14 17:44:38 +0800

    t

--- 41-钟小辉-东莞/第二周/Test2/city_functions.py
+++
@@ -1,8 +0,0 @@
-
-
-
-def set_City_name(city,country,population =""):
-    print(f"{city},{country},{population}")
-    return f"{city},{country}"
-
-set_City_name("GuangZhou","China")
--- 41-钟小辉-东莞/第二周/Test2/颜色通道截取.py
+++
@@ -1,5 +0,0 @@
-import  numpy as np
-import  matplotlib.pyplot as plt
-import  cv2
-
-b,g,r = cv2.split()
